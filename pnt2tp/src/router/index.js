7024bd4c344c131d5a1a2ec9b2924c6b91dd0c36
Babau <bauti707@gmail.com>
2026-05-05 21:57:41 -0300

    Ejemplo de router

---
+++ pnt2tp/src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Perfil from '../views/Perfil.vue'
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+    {
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+
+
+    {
+        path: '/perfil/:id',
+        name: 'Perfil',
+        component: Perfil
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFound
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
