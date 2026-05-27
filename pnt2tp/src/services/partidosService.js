f7b15eaec11982817d5bdd9b080eb56597747638
Juan Manuel Gimenez <juann_m@hotmail.com>
2026-05-26 21:37:26 -0300

    Agrega partidos, banderas e imagen de inicio

--- pnt2tp/src/services/partidosService.js
+++ pnt2tp/src/services/partidosService.js
@@ -1,5 +1,11 @@
-import { partidosMock } from '../data/partidosMock'
+const API_URL = 'https://6a1627131b90031f81b0b808.mockapi.io/api/v1/partidos'
 
 export async function obtenerPartidos() {
-  return partidosMock
+  const respuesta = await fetch(API_URL)
+
+  if (!respuesta.ok) {
+    throw new Error('No se pudieron obtener los partidos')
+  }
+
+  return await respuesta.json()
 }
--- pnt2tp/src/views/Home.vue
+++ pnt2tp/src/views/Home.vue
@@ -1,14 +1,33 @@
 <script setup>
-
+import imagenInicio from '@/assets/img/prode-mundial-2026.png'
 </script>
 
 <template>
-  <h1 class="title">Prode Mundial 2026</h1>
+  <main class="home">
+    <section class="hero">
+      <img
+        :src="imagenInicio"
+        alt="Prode Mundial 2026"
+        class="hero-img"
+      />
+    </section>
+  </main>
 </template>
 
 <style scoped>
-.title {
-  text-align: center;
-  margin-left: 10px;
+.home {
+  min-height: 100vh;
+  background-color: #050914;
+}
+
+.hero {
+  width: 100%;
+  overflow: hidden;
+}
+
+.hero-img {
+  width: 100%;
+  height: auto;
+  display: block;
 }
 </style>
